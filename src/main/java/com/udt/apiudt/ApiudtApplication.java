f065dbdef094b9d9215f3c5cfca2e3f2686cb9ac
hdzdesign <chcandroistudio@gmail.com>
2018-10-22 00:50:43 +0200

    Initial commit

---
+++ src/main/java/com/udt/apiudt/ApiudtApplication.java
@@ -0,0 +1,12 @@
+package com.udt.apiudt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiudtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiudtApplication.class, args);
+	}
+}
---
+++ src/test/java/com/udt/apiudt/ApiudtApplicationTests.java
@@ -0,0 +1,16 @@
+package com.udt.apiudt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiudtApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
