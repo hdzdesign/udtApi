14d32ef96a8f26b1db15309b05de0106ffff4040
hdzdesign <chcandroistudio@gmail.com>
2018-10-24 23:08:28 +0200

    api impl

---
+++ src/main/java/com/udt/apiudt/configure/EquipoImpl.java
@@ -0,0 +1,50 @@
+package com.udt.apiudt.configure;
+
+import com.example.generated.api.EquiposApi;
+import com.example.generated.model.Equipo;
+import com.example.generated.model.Equipos;
+import com.example.generated.model.Jugador;
+import com.example.generated.model.Jugadores;
+import org.springframework.http.ResponseEntity;
+
+public class EquipoImpl implements EquiposApi {
+    @Override
+    public ResponseEntity<Jugador> equiposEquipoIdEquiposJugadorIdGet(String equipoId, String jugadorId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Void> equiposEquiposIdDelete() {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Equipo> equiposEquiposIdGet(String equiposId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Jugadores> equiposEquiposIdJugadoresGet(String equiposId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Jugador> equiposEquiposIdJugadoresPost(String jugadorId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Equipo> equiposEquiposIdPut(String equiposId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Equipos> equiposGet() {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Void> equiposPost(Equipo equipo) {
+        return null;
+    }
+}
